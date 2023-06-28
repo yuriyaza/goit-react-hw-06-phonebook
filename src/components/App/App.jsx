7fe22a7a8de11270bac40b0b92a2f219d0a7a81d
yuriyaza <yuriy.aza@gmail.com>
2023-06-29 00:16:10 +0300

    Update 1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,6 @@
-import { useState, useEffect, useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { contactSlice } from 'redux/contactSlice';
 import { Notify } from 'notiflix';
 
 import { ContactForm } from 'components/ContactForm/ContactForm';
@@ -7,45 +9,33 @@ import { Filter } from 'components/Filter/Filter';
 import css from './App.module.css';
 
 Notify.init({ showOnlyTheLastOne: true, clickToClose: true });
-const STORAGE = 'contacts';
 
 export const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    return (
-      JSON.parse(localStorage.getItem(STORAGE)) ?? [
-        // Якщо в Local Storage даних немає -
-        // використовуємо початкові дані для демонстрації
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ]
-    );
-  });
-  const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    localStorage.setItem(STORAGE, JSON.stringify(contacts));
-  }, [contacts]);
+  const contacts = useSelector(state => state.contactState.contacts);
+  const filter = useSelector(state => state.contactState.filter);
+  const dispatch = useDispatch();
 
   const onAddContact = newContact => {
     const nameList = contacts.map(contact => contact.name.toLowerCase());
-
+    
     if (nameList.includes(newContact.name.toLowerCase())) {
       Notify.failure(`${newContact.name} is already in contacts`);
       return;
     }
-    setContacts(contacts => [...contacts, newContact]);
+    dispatch(contactSlice.actions.addContacts(newContact));
   };
 
   const onDeleteContact = id => {
-    const contactListWithoutDeleted = contacts.filter(contact => contact.id !== id);
-    setContacts(contactListWithoutDeleted);
+    dispatch(contactSlice.actions.deleteContact(id));
   };
 
-  const filteredContacts = useMemo(() => {
-    return contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()));
-  }, [contacts, filter]);
+  const onSetFilter = filter => {
+    dispatch(contactSlice.actions.setFilter(filter));
+  };
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
   return (
     <div className={css.container}>
@@ -53,7 +43,7 @@ export const App = () => {
       <ContactForm onAddContact={onAddContact} />
 
       <h2 className={css.subtitle}>Contacts</h2>
-      <Filter filter={filter} setFilter={setFilter} />
+      <Filter filter={filter} setFilter={onSetFilter} />
 
       <ContactList contacts={filteredContacts} onDeleteContact={onDeleteContact} />
     </div>
